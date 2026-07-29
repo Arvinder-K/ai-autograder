5ef1bd99031d61610316d321801e3d7b41155513
Arvinder-K <niit.arvinder@gmail.com>
2026-07-29 16:22:01 +0530

    Hide PHP deprecation warnings

--- api/index.php
+++ api/index.php
@@ -1,3 +1,5 @@
 <?php
-// Forward Vercel requests to normal index.php
+// Hide deprecation warnings that show up on PHP 8.4
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+
 require __DIR__ . '/../public/index.php';
